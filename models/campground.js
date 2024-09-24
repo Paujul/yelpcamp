71b3e763a9a4865fd41876cff29f5420f3089d1e
Fauzul Ashari <p4ujul@gmail.com>
2024-09-24 14:05:49 +0700

    Add models

--- models/campground.js
+++ models/campground.js
@@ -1,11 +1,30 @@
 const mongoose = require("mongoose")
+const Review = require("./review")
 const Schema = mongoose.Schema
 
 const CampgroundSchema = new Schema({
   title: String,
-  price: String,
+  image: String,
+  price: Number,
   description: String,
   location: String,
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review", // Review model
+    },
+  ],
+})
+
+// <schema>.post(delete) = Post delete mo ngapain?
+CampgroundSchema.post("findOneAndDelete", async function (campground) {
+  if (campground) {
+    await Review.deleteMany({
+      _id: {
+        $in: campground.reviews,
+      },
+    })
+  }
 })
 
 module.exports = mongoose.model("Campground", CampgroundSchema)
