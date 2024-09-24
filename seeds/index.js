9ae1abb76c16067d7a42421d88b8d75d069639ce
Fauzul Ashari <p4ujul@gmail.com>
2024-09-24 14:23:46 +0700

    Seed: Add price & image placeholder

--- seeds/index.js
+++ seeds/index.js
@@ -17,11 +17,15 @@ const seedDB = async () => {
   await Campground.deleteMany({})
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000)
-
+    const price = Math.floor(Math.random() * 20) + 10
     // Seed DB pake cities yg dirandomin sm seedHelper buat randomize titlenya.
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: `https://picsum.photos/400?random=${Math.random()}`,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis aliquid impedit veritatis tenetur quos, nesciunt quo quis modi autem ducimus laborum dolor dolore labore deserunt quia debitis, sed eius! Earum?",
+      price,
     })
     await camp.save()
   }
